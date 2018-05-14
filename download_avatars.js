671cc96dca2671ffdc120aba37c478b3ad229c9d
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 22:36:55 +0000

    updated js file with headers

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,23 @@
 var request = require('request');
+var token = ("secrets").GITHUB_TOKEN;
 
 console.log('Welcome to the Github Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token
+    }
+  };
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
 
-getRepoContributors('jquery', 'jquery', function(err, result){
+getRepoContributors('lighthouse-labs', 'tweeter', function(err, result){
   console.log("Errors:", err);
   console.log("Result:", result);
 });
