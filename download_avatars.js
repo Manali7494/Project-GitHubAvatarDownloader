dcb5b05fb2a0e2dad2ad75648afd50792f81a518
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-14 23:00:48 +0000

    added function to download image by url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = ("secrets").GITHUB_TOKEN;
+var fs = require('fs');
 
 console.log('Welcome to the Github Avatar Downloader');
 
@@ -25,3 +26,13 @@ getRepoContributors('jquery', 'jquery', function(err, parsedBody){
     console.log(element["avatar_url"]);
   });
 });
+
+function downloadImageByURL(url, filePath){
+  request.get(url)
+    .on('error', function(err){
+      console.log(err);
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
